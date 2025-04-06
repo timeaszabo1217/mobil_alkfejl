0b0aa7f9f30392569f7ae9579783e2bc48adb48b
Szabó Tímea <timeaszabo1217@gmail.com>
2025-04-06 16:53:54 +0200

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    id("com.google.gms.google-services")
+}
+
+android {
+    namespace = "com.example.furniturewebshop"
+    compileSdk = 35
+
+    buildToolsVersion = "35.0.0"
+
+    defaultConfig {
+        applicationId = "com.example.furniturewebshop"
+        minSdk = 35
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+    implementation(platform("com.google.firebase:firebase-bom:33.12.0"))
+    implementation("com.google.firebase:firebase-analytics:22.4.0")
+    implementation("com.google.firebase:firebase-auth:23.2.0")
+    implementation("com.google.android.gms:play-services-auth:21.3.0")
+}
