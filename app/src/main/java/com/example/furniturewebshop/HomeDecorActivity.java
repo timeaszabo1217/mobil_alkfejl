83716511dc706f04f612a3773551bcfd715d70c8
Szabó Tímea <timeaszabo1217@gmail.com>
2025-04-13 20:04:35 +0200

    menu items

--- app/src/main/java/com/example/furniturewebshop/HomeDecorActivity.java
+++ app/src/main/java/com/example/furniturewebshop/HomeDecorActivity.java
@@ -1,4 +1,15 @@
 package com.example.furniturewebshop;
 
-public class HomeDecorActivity {
+import android.os.Bundle;
+
+public class HomeDecorActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.content_frame, new MainFragment())
+                    .commit();
+        }
+    }
 }
