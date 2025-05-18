692a23d22a8dac66f0b3f9d630b3f2b9e1b0def1
Szabó Tímea <timeaszabo1217@gmail.com>
2025-05-18 19:19:03 +0200

    Initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.firebase.firestore)
+    implementation(libs.firebase.storage)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
@@ -45,4 +47,5 @@ dependencies {
     implementation("com.google.firebase:firebase-auth:23.2.0")
     implementation("com.google.android.gms:play-services-auth:21.3.0")
     implementation("androidx.cardview:cardview:1.0.0")
+    implementation ("com.squareup.picasso:picasso:2.8")
 }
